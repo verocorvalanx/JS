1bc16c3a6efdf8aaa15c1abadcc4ade606632f0f
vero <veroo.corvalan@gmail.com>
2023-08-08 17:44:23 -0300

    c
    c

--- Clase06/main.js
+++ Clase06/main.js
@@ -1,6 +1,6 @@
-let dataPersona =[];
-const addDataPersona = (firstName,lastName,birthday,address,status,dni,condition=true) =>{
-    let person ={
+let dataPersona = [];
+const addDataPersona = (firstName, lastName, birthday, address, status, dni, condition = true) => {
+    let person = {
         dni,
         firstName,
         lastName,
@@ -8,16 +8,16 @@ const addDataPersona = (firstName,lastName,birthday,address,status,dni,condition
         address,
         status
     }
-    if(condition){
+    if (condition) {
         dataPersona.push(person);
     }
-    else{
+    else {
         dataPersona.unshift(person);
     }
 
 }
 
-const addFormPerson = () =>{
+const addFormPerson = () => {
     let firstName = document.getElementById("fistName");
     let lastName = document.getElementById("lastName");
     let birthday = document.getElementById("birthday");
@@ -25,20 +25,22 @@ const addFormPerson = () =>{
     let status = document.getElementById("status");
     let dni = document.getElementById("dni");
     console.log(firstName.value);
-    addDataPersona(firstName.value,lastName.value,birthday.value,address.value,status.value,dni.value);
-    firstName.value ='';
-    lastName.value='';
-    birthday.value='';
-    address.value='';
-    status.value='';
-    dni.value='';
+    addDataPersona(firstName.value, lastName.value, birthday.value, address.value, status.value, dni.value);
+    firstName.value = '';
+    lastName.value = '';
+    birthday.value = '';
+    address.value = '';
+    status.value = '';
+    dni.value = '';
     Swal.fire('Saved!', '', 'success')
     firstName.focus();
-    
-    
+
+
     console.table(dataPersona);
 }
 
 /** 1.- EVITAR QUE EL USUARIO INGRESE DATOS VACIOS.
  *  2.- EVITAR DUPLICIDAD DE DATOS POR EL DNI
  */
+
+
