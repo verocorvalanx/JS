0545ab806c4f2c06bc615d62d31511bcafcd1af7
vero <veroo.corvalan@gmail.com>
2023-08-09 23:14:51 -0300

    c
    c

--- Clase06/main.js
+++ Clase06/main.js
@@ -24,23 +24,32 @@ const addFormPerson = () => {
     let address = document.getElementById("address");
     let status = document.getElementById("status");
     let dni = document.getElementById("dni");
+
+
     console.log(firstName.value);
-    addDataPersona(firstName.value, lastName.value, birthday.value, address.value, status.value, dni.value);
-    firstName.value = '';
-    lastName.value = '';
-    birthday.value = '';
-    address.value = '';
-    status.value = '';
-    dni.value = '';
-    Swal.fire('Saved!', '', 'success')
-    firstName.focus();
+
+    if (firstName.value.trim() === "" || lastName.value.trim() === "" || birthday.value.trim() === "" || address.value.trim() === "" || status.value.trim() === "" || dni.value.trim() === "") {
+        alert("Complete todos los campos");
+    } else {
+        addDataPersona(firstName.value, lastName.value, birthday.value, address.value, status.value, dni.value);
+        firstName.value = '';
+        lastName.value = '';
+        birthday.value = '';
+        address.value = '';
+        status.value = '';
+        dni.value = '';
+        Swal.fire('Saved!', '', 'success')
+        firstName.focus();
+        console.table(dataPersona);
+
+    }
 
 
-    console.table(dataPersona);
 }
+
+
 
 /** 1.- EVITAR QUE EL USUARIO INGRESE DATOS VACIOS.
  *  2.- EVITAR DUPLICIDAD DE DATOS POR EL DNI
  */
-
 
