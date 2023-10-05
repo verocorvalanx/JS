b850192b7fbdb25bc2889f29f4592e51bd00f391
vero <veroo.corvalan@gmail.com>
2023-10-04 21:52:09 -0300

    c
    c

---
+++ Ejercicios 1b/dom/main.js
@@ -0,0 +1,27 @@
+const openButton = document.querySelector('#open-btn');
+
+
+const addButton = document.querySelector('#add-btn');
+
+
+const removeButton = document.querySelector('#rmv-btn');
+
+const main = document.querySelector('main')
+
+
+const addParagraphToMain = () => {
+    const elementToAppend = document.createElement('p')
+    elementToAppend.textContent = 'Este párrafo lo creé con JS'
+    main.appendChild(elementToAppend)
+
+}
+
+addButton.addEventListener('click', addParagraphToMain)
+
+
+const removeLasElementFromMain = () => {
+    const lastP = main.lastChild
+    main.removeChild(lastP)
+}
+
+removeButton.onclick = removeLasElementFromMain;
