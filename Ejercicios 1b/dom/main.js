567b7f4c2f4f24f00f8193df9d1654a7f5c4d372
vero <veroo.corvalan@gmail.com>
2023-10-04 22:28:47 -0300

    c
    c

--- Ejercicios 1b/dom/main.js
+++ Ejercicios 1b/dom/main.js
@@ -3,10 +3,11 @@ const openButton = document.querySelector('#open-btn');
 
 const addButton = document.querySelector('#add-btn');
 
-
 const removeButton = document.querySelector('#rmv-btn');
 
-const main = document.querySelector('main')
+const popUp = document.querySelector('.pop-up');
+
+const main = document.querySelector('main');
 
 
 const addParagraphToMain = () => {
@@ -23,4 +24,25 @@ addButton.addEventListener('click', addParagraphToMain)
 removeButton.onclick = () => {
     const lastP = main.lastChild
     main.removeChild(lastP)
+}
+
+/*
+openButton.onclick = () => {
+    const currentText = openButton.textContent.trim()
+    if (currentText === 'Abrir') {
+        popUp.classList.toggle('closed')
+        openButton.textContent = 'Cerrar'
+    } else {
+        popUp.classList.toggle('closed')
+        openButton.textContent = 'Abrir'
+    }
+    popUp
+}
+*/
+//mejor hecha, rework:
+
+openButton.onclick = () => {
+    let {textContent} = openButton;
+    textContent.trim() === 'Abrir' ? textContent = 'Cerrar' : textContent = 'Abrir'
+    popUp.classList.toggle('closed')
 }
