58b8343007e10288673226c127cf1c8c07c8140d
vero <veroo.corvalan@gmail.com>
2023-10-10 20:58:00 -0300

    c
    c

---
+++ Ejercicios 1b/agenda/script.js
@@ -0,0 +1,10 @@
+//a partir del formulario, generar una instancia de la clase
+//Contact, añadirla al final del array, y actualizar el DOM para que
+//sincronizando con el array PHONEBOOK
+
+
+class Contact {}
+
+const PHONEBOOK = [];
+
+const addButton = document.querySelector('#add-btn')
