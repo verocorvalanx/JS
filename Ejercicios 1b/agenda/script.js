f59405fdfe63c4935287c4394bbb19fa0d77edc2
vero <veroo.corvalan@gmail.com>
2023-10-11 22:25:02 -0300

    c
    c

--- Ejercicios 1b/agenda/script.js
+++ Ejercicios 1b/agenda/script.js
@@ -3,8 +3,43 @@
 //sincronizando con el array PHONEBOOK
 
 
-class Contact {}
-
+class Contact {
+    constructor(name, tel, email) {
+        this.name = name
+        this.tel = tel
+        this.email = email
+    }
+    add() {
+        PHONEBOOK.push(this)
+    }
+}
+//así phonebook tiene los métodos de array:
 const PHONEBOOK = [];
 
-const addButton = document.querySelector('#add-btn')
+const addButton = document.querySelector('#add-btn');
+
+const nameInput = document.querySelector('#name');
+
+const telInput = document.querySelector('#tel');
+
+const mailInput = document.querySelector('#mail');
+
+//crear el contacto
+
+addButton.addEventListener('click', createContact)
+
+const createContact = () => {
+    const name = nameInput.value;
+    const tel = telInput.value;
+    const mail = mailInput.value;
+
+    let newContact = new Contact(name, tel, mail)
+
+    console.log(newContact)
+}
+
+
+
+//enviarlo al array
+
+//actualizar el dom
