755dbc342d409e11d99e88063b941db3013936f8
vero <veroo.corvalan@gmail.com>
2023-10-12 20:18:19 -0300

    c
    c

--- Ejercicios 1b/agenda/script.js
+++ Ejercicios 1b/agenda/script.js
@@ -1,6 +1,6 @@
 //a partir del formulario, generar una instancia de la clase
 //Contact, añadirla al final del array, y actualizar el DOM para que
-//sincronizando con el array PHONEBOOK
+//esté sincronizado con el array PHONEBOOK
 
 
 class Contact {
@@ -13,6 +13,7 @@ class Contact {
         PHONEBOOK.push(this)
     }
 }
+
 //así phonebook tiene los métodos de array:
 const PHONEBOOK = [];
 
@@ -24,22 +25,87 @@ const telInput = document.querySelector('#tel');
 
 const mailInput = document.querySelector('#mail');
 
+const phonebookDOM = document.querySelector('#agenda')
+
 //crear el contacto
 
-addButton.addEventListener('click', createContact)
 
-const createContact = () => {
+
+const createContact = (e) => {
+
+    e.preventDefault()
     const name = nameInput.value;
     const tel = telInput.value;
     const mail = mailInput.value;
 
-    let newContact = new Contact(name, tel, mail)
 
-    console.log(newContact)
+    //si estan vacios
+    if (!name || !tel || !mail) {
+        alert('Hubo un error verificando los datos')
+    } else {
+        let newContact = new Contact(name, tel, mail)
+        checkIfContactExists(newContact)
+
+
+    }
+    //vaciar los input
+    document.querySelector('form').reset()
+
+}
+
+
+function checkIfContactExists(obj) {
+    //comprobación si se repiten
+    //vamos a usar el método SOME del prototipo de Array que
+    //devuelve true si encuentra al mmenos un elemento
+    //que cumpla con la condición y false si no encuentra ninguno
+    if (PHONEBOOK.some((cntct) => cntct.email ===
+        obj.email)) {
+        alert('Ya está agendado')
+    } else {
+
+        // accedo a su método add, el cual lo sabe pushear
+        // a PHONEBOOK
+        obj.add()
+        console.log(obj)
+        updateDOM(obj)
+    }
+
 }
 
 
 
-//enviarlo al array
-
 //actualizar el dom
+function updateDOM(obj) {
+    //la desestructuracion me permite
+    //guardar el valor de propiedades
+    //de un obj en variables del mismo nombre
+    const { name, tel, email } = obj
+
+    let newContact = document.createElement('div')
+
+    newContact.classList.add('contacto')
+
+    const nameNode = document.createElement('h3')
+    nameNode.textContent = name;
+    const telNode = document.createElement('span')
+    telNode.textContent = tel;
+    const mailNode = document.createElement('span')
+    mailNode.textContent = email;
+
+
+    newContact.appendChild(nameNode)
+    newContact.appendChild(mailNode)
+    newContact.appendChild(telNode)
+
+
+    agenda.appendChild(newContact)
+}
+
+
+
+addButton.onclick = (e) => {
+    createContact(e);
+
+
+}
