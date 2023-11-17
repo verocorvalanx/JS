20f4e28cf8be7133a9676d1c48f8a9b3e053ab7e
vero <veroo.corvalan@gmail.com>
2023-11-16 23:01:55 -0300

    c
    c

--- 1b/fetch/main.js
+++ 1b/fetch/main.js
@@ -3,3 +3,11 @@ const CHARACTERS = []
 const url = 'https://rickandmortyapi.com/api/character'
 
 const body = document.querySelector('body')
+
+const fetchData = () => {
+    fetch(url)
+        .then((res) => { })
+        .catch((err) => { })
+        .finally()
+
+} 
